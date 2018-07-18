74dfc8d12d50c4abff7a2bb268775626592b41da
Leviathan143 <Leviathan143@users.noreply.github.com>
2018-07-18 22:06:55 +1200

    Initial Commit

---
+++ Backend/HotPytatoBackend.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotPytato
+{
+    public class HotPytatoBackend
+    {  
+        public static void Init()
+        {
+
+        }
+    }
+}
---
+++ WindowsUI/HotPytatoWindowsUI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HotPytato.UI.Windows;
+
+namespace HotPytato
+{
+    static class HotPytatoWindowsUI
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            HotPytato.HotPytatoBackend.Init();
+            Application.Run(new MainWindow());
+        }
+    }
+}
